14e6db3d7173e51b354694fef59d511be1aaeadb
robawills <rob@kbamarketing.co.uk>
2018-01-15 21:43:00 +0000

    Background color changes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -21,3 +21,26 @@ $window.on('scroll', function(){
   nav.toggleClass('hidden', scrollTop > prev);
   prev = scrollTop;
 });
+
+
+// Background colours
+
+var darkgrey = "#171717",
+	white = "#ffffff",
+	lightgrey = "#F7F6F2";
+
+$(window).on("scroll touchmove", function() {
+		if ($(document).scrollTop() >= $("#darkgrey").position().top) {
+				$('.hero').css('background', $("#darkgrey").attr("data-color"));
+
+		};
+		if ($(document).scrollTop() > $("#white").position().top) {
+				$('.hero').css('background', $("#white").attr("data-color"))
+		};
+		if ($(document).scrollTop() > $("#lightgrey").position().top) {
+
+				$('.services').css('background', $("#lightgrey").attr("data-color"))
+		};
+
+});
+jackHarnerSig("light")
