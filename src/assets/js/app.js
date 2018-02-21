644c4bb8713d73fa0bf66727e06cfd4884ba86c9
robawills <rob@kbamarketing.co.uk>
2018-02-21 21:58:04 +0000

    Making the background work WHOOP

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -9,32 +9,32 @@ import Foundation from 'foundation-sites';
 //import './lib/foundation-explicit-pieces';
 
 
-$(document).foundation();
+// $(document).foundation();
 
 
-var prev = 0;
-var $window = $(window);
-var nav = $('.head');
+// var prev = 0;
+// var $window = $(window);
+// var nav = $('.head');
 
-$window.on('scroll', function(){
-  var scrollTop = $window.scrollTop();
-  nav.toggleClass('hidden', scrollTop > prev);
-  prev = scrollTop;
-});
+// $window.on('scroll', function(){
+//   var scrollTop = $window.scrollTop();
+//   nav.toggleClass('hidden', scrollTop > prev);
+//   prev = scrollTop;
+// });
 
 
 
-$(window).scroll(function(event){
+// $(window).scroll(function(event){
 
-	var yOffset = window.pageYOffset;
-	var breakpoint = 150;
-	if (yOffset > breakpoint){
-	  $(".bg-gray").addClass('active');
-	}else{
-	  $(".bg-gray").removeClass('active');
-	}
+// 	var yOffset = window.pageYOffset;
+// 	var breakpoint = 150;
+// 	if (yOffset > breakpoint){
+// 	  $(".bg-gray").addClass('active');
+// 	}else{
+// 	  $(".bg-gray").removeClass('active');
+// 	}
   
-  });
+//   });
 
 
 
@@ -55,5 +55,31 @@ $(window).scroll(function(event){
 
 
 
+function querySelectorArray(query, root) {
+  return Array.prototype.slice.call((root || document).querySelectorAll(query));
+}
+
+var elements = querySelectorArray("[data-scroll]"),
+  bg = document.getElementById("content"),
+  container = document.querySelector(".scroll_container");
+
+elements.forEach(function(color) {
+  var group = color.getAttribute("data-scroll"),
+    sections = querySelectorArray("[data-" + group + "]");
+
+  container.addEventListener("scroll", function() {
+    var lastSection = false;
+    sections.forEach(function(section) {
+      var offset = section.getBoundingClientRect();
+      //the magic
+      if (
+        offset.top - window.innerHeight / 2 < 0 &&
+        offset.top > -offset.height
+      )
+        lastSection = section.getAttribute("data-" + group + "");
+    });
+    bg.className = lastSection || "";
+  });
+});
 
 
